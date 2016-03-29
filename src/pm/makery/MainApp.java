f5edc9db991371f6a9c287d3799ab4222312af7c
Joakim <joakim.0565@gmail.com>
2016-03-29 23:51:35 +0200

    Restored rootlayout

--- src/pm/makery/MainApp.java
+++ src/pm/makery/MainApp.java
@@ -44,7 +44,7 @@ public class MainApp extends Application {
 		try {
 			// Load root layout from fxml file.
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/PlaneLayout.fxml"));
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
 			rootLayout = loader.load();
 			rootScene = new Scene(rootLayout);
 
