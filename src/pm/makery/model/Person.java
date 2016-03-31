f76b50ed8873a65eb79b4c29cf51719f4cd0831f
malindahl <malin.dahl_@hotmail.com>
2016-03-31 23:30:59 +0200

    shell of Person

---
+++ src/pm/makery/model/Person.java
@@ -0,0 +1,20 @@
+package pm.makery.model;
+
+import java.util.*;
+import pm.makery.model.Subject;
+import pm.makery.model.Assignment;
+
+public class Person {
+
+	private String username = "";
+	private String password = "";
+	private HashMap<Subject, ArrayList<Assignment>> subjectsAndAssignments = new HashMap();
+
+	public Person(String userName) {
+
+	}
+
+	public HashMap<Subject, ArrayList<Assignment>> getSubjectsAndAssignments() {
+		return subjectsAndAssignments;
+	}
+}
